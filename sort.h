345b98ae22a1d6b009fb02ea88cc4c969d5fa8e0
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-08-07 13:37:06 +0200

    chore: Update sort.h to include necessary header files

--- sort.h
+++ sort.h
@@ -1,6 +1,10 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -23,4 +27,4 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
-#endif
+#endif /* SORT_H */
